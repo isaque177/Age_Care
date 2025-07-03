ed0d6712e1fe299d948a6d8ea3403b2bd84a3481
isaque177 <0001083214@senaimgaluno.com.br>
2025-07-02 23:24:48 -0300

    Update UsuarioController.java

--- Age_Care-main/src/main/java/com/example/AgeCare/controller/UsuarioController.java
+++ Age_Care-main/src/main/java/com/example/AgeCare/controller/UsuarioController.java
@@ -1,22 +1,19 @@
 package com.example.AgeCare.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.AgeCare.model.Usuario;
-import com.example.AgeCare.service.UsuarioService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
+import com.example.AgeCare.model.UsuarioModel;
+import com.example.AgeCare.service.UsuarioService;
 
 
 
@@ -30,28 +27,32 @@ public class UsuarioController {
     private UsuarioService service;
 
     @PostMapping
-    public ResponseEntity<Usuario> registrar (@RequestBody Usuario usuario){
-       return ResponseEntity.ok(service.registrarUsuario(usuario)); 
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> buscar (@PathVariable Long id){
-        return service.buscarPorid(id)
-        .map(ResponseEntity:: ok)
-        .orElse(ResponseEntity.notFound().build());
-    }
-  
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @RequestBody Usuario usuario){
-        return ResponseEntity.ok(service.atualizarUsuario(id, usuario));
-
+    public ResponseEntity<UsuarioModel> salvar(@RequestBody UsuarioModel usuario){
+        return ResponseEntity.ok(service.salvar(usuario));
     }
 
     @GetMapping
-    public ResponseEntity <List<Usuario>> listar(){
-        return ResponseEntity.ok(service.listarUsuarios());
-
+    public ResponseEntity <List<UsuarioModel>> listar() {
+        return ResponseEntity.ok(service.listar());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioModel> buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioModel> atualizar(@PathVariable Long id,@RequestBody UsuarioModel usuario) {
+        return ResponseEntity.ok(service.atualizar(id, usuario));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
     
 }
