7463e91c78186c6d2eb3cbbfe414c30b04218082
isaque177 <0001083214@senaimgaluno.com.br>
2025-07-02 23:23:25 -0300

    Update ProfissionalModel.java

--- Age_Care-main/src/main/java/com/example/AgeCare/model/ProfissionalModel.java
+++ Age_Care-main/src/main/java/com/example/AgeCare/model/ProfissionalModel.java
@@ -1,9 +1,49 @@
 package com.example.AgeCare.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "profissionais")
+@Getter
+@Setter
+
 public class ProfissionalModel {
-//id colocado apenas para não acusar erro na classe enderecoModel
-    //A classe ProfissionalModel ainda está a ser desenvolvida, por isso não tem atributos definidos
-    //Quando estiver completa, deverá conter atributos como nome, especialidade, etc.
-private Long id;
-    //Ainda a fazer
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer experienciaAnos;
+    private String formacao;
+    private String especializacoes;
+    private BigDecimal valorHora;
+    private BigDecimal valorDiaria;
+    private Boolean disponibilidade24h ;
+    private BigDecimal raioAtendimentoKm;
+    private String biografia;
+    private String curriculoUrl;
+    private Double notaMedia ;
+    private Integer totalAvaliacoes ;
+    private Integer totalAtendimentos ;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_aprovacao", columnDefinition = "ENUM('pendente', 'aprovado', 'rejeitado') DEFAULT 'pendente'")
+    private StatusAprovacao statusAprovacao = StatusAprovacao.pendente;
+    
+    
+    public enum StatusAprovacao {
+        pendente, aprovado, rejeitado
+    }
+   
 }
