0c35f7fb925539e3c40a2717157d7726246b57b1
isaque177 <0001083214@senaimgaluno.com.br>
2025-07-02 23:22:43 -0300

    Update UsuarioModel.java

--- Age_Care-main/src/main/java/com/example/AgeCare/model/UsuarioModel.java
+++ Age_Care-main/src/main/java/com/example/AgeCare/model/UsuarioModel.java
@@ -1,5 +1,37 @@
 package com.example.AgeCare.model;
 
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+
 public class UsuarioModel {
+
+      public UsuarioModel() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+    private String email;
+    private String senhaHash;
+    private String telefone;
+    private String celular;
+    private String fotoPerfil;
+    private LocalDate dataNascimento;
+    private String cpf;
+    private LocalDate dataCriacao;
+    private LocalDate dataAtualizacao;
+
 
 }
